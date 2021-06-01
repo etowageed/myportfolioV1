b50cf5e80998c544ed940db505ef1fa8cc9861fd
Gideon Etowa <etowagideon@gmail.com>
2021-06-01 15:12:10 +0100

    more edits

---
+++ js/main.js
@@ -0,0 +1,7 @@
+function myFunction() {
+  document.getElementById("over-lay").style.visibility = "visible";
+}
+
+function closeBtn() {
+  document.getElementById("over-lay").style.visibility = "hidden";
+}
