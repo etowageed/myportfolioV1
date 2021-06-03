a82da25f0a176f3c135863959070b9e2d40ec44a
Gideon Etowa <etowagideon@gmail.com>
2021-06-03 14:44:26 +0100

    fixed the nav, darkmode toggle icons and more

--- js/main.js
+++ js/main.js
@@ -1,4 +1,12 @@
 function darkMode() {
   var nightMode = document.getElementById("container-div");
   nightMode.classList.toggle("dark-mode");
+  var nightModeNav = document.getElementById("nav-navbar");
+  nightModeNav.classList.toggle("dark-mode-nav");
+
+  if (document.getElementById("moon").className == "fa fa-moon-o fa-lg") {
+    document.getElementById("moon").className = "fa fa-sun-o fa-lg";
+  } else {
+    document.getElementById("moon").className = "fa fa-moon-o fa-lg";
+  }
 }
