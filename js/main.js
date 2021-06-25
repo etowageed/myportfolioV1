68c93c726cea5580a882767f326a88372fa5e6ee
Gideon Etowa <etowagideon@gmail.com>
2021-06-25 06:13:50 +0100

    worked on the sliding nav

--- js/main.js
+++ js/main.js
@@ -1,12 +1,26 @@
 function darkMode() {
-  var nightMode = document.getElementById("body");
+  let nightMode = document.getElementById("body");
   nightMode.classList.toggle("dark-mode");
-  var nightModeNav = document.getElementById("nav-navbar");
+  let nightModeNav = document.getElementById("nav-navbar");
   nightModeNav.classList.toggle("dark-mode-nav");
 
   if (document.getElementById("moon").className == "fa fa-moon-o fa-lg") {
     document.getElementById("moon").className = "fa fa-sun-o fa-lg";
   } else {
     document.getElementById("moon").className = "fa fa-moon-o fa-lg";
+  }
+}
+
+let menuBtn = document.querySelector(".btn-menu");
+let overlayAside = document.getElementById("slide-nav");
+let closeBtn = document.getElementById("btn-close");
+
+function openOverlay() {
+  overlayAside.style.visibility = "visible";
+}
+
+function closeOverlay() {
+  if (openOverlay == true) {
+    overlayAside.style.visibility = "hidden";
   }
 }
