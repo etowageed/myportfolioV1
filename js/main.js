7462e01f935902880926f70b88c2b9fa5229ed8a
Gideon Etowa <etowagideon@gmail.com>
2021-10-17 18:26:12 +0100

    final fixes

--- js/main.js
+++ js/main.js
@@ -3,25 +3,18 @@ const overlayAside = document.querySelector(".nav-overlay-nav");
 const closeBtn = document.getElementById("btn-close");
 const dayMode = document.getElementById("body");
 const dayLightNav = document.querySelector(".navbar");
-const lightDivs = document.querySelectorAll("div.rect, div.tiles");
-// const changeBtn = document.getElementsByClassName("toggleBtn");
-
 /* toggling between modes */
 function lightMode() {
   dayMode.classList.toggle("light-mode");
-  dayLightNav.classList.toggle("light-mode-nav");
  
 
   if (document.getElementById("moon").className == "fa fa-sun-o fa-lg") {
     document.getElementById("moon").className = "fa fa-moon-o fa-lg";
+
   } else {
     document.getElementById("moon").className = "fa fa-sun-o fa-lg";
   }
 }
-
-// changeBtn.addEventListener('click', function(){
-//   lightDivs.classList.toggle("rect-White");
-// })
 
 /* overlay nav */
 function openOverlay() {
